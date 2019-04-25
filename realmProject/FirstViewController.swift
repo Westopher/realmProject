1218a7efe90b41c94064b25a354750332f249ca0
West Kraemer <kraemerwest@gmail.com>
2019-04-25 12:21:41 -0400

    first commit

---
+++ realmProject/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  realmProject
+//
+//  Created by West Kraemer on 4/25/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
